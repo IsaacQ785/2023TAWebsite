c5af6604061676b3859807ac83f9d363d1cc069f
IsaacQ785 <70641752+IsaacQ785@users.noreply.github.com>
2023-02-09 18:51:47 +0000

    add fetching tasks

---
+++ serveApp.ts
@@ -0,0 +1,15 @@
+// import router from "./src/api/server";
+// import bodyParser from "body-parser";
+// import cors from "cors";
+// import express, { Application, Router } from "express";
+// const path = require("path");
+
+// const app: Application = express();
+// const PORT = process.env.PORT || 8088;
+
+// app.use(router);
+// app.use(cors(), bodyParser.json())
+// app.set("port", PORT);
+// app.use(express.static(path.join(__dirname, "public")));
+// app.listen();
+
--- src/api/database-tasks/tasks.ts
+++ src/api/database-tasks/tasks.ts
@@ -1,7 +1,11 @@
 import { Db, MongoClient } from "mongodb";
+import { exit } from "process";
 import { getTicker } from "./getTickerData";
 import { processStream } from "./processRawData";
 import { IEODPoint } from "./types/IEODPoint";
+import { IRawFormat } from "./types/IRawFormat";
+import tickers from "./stocks_held_edx.json";
+// const tickers = ["A", "AA", "AAL", "AAP", "AAPL", "ABB", "ABBV"];
 
 const url =
   "mongodb+srv://IsaacQuinton:Test1234@cluster0.muipr.mongodb.net/?retryWrites=true&w=majority";
@@ -14,16 +18,23 @@ const connect = async () => {
   return db;
 };
 
-const initialize = async () => {
-  let db = await connect();
-  db.createCollection("S&P500TickerData", {
-    timeseries: {
-      timeField: "date",
-      metaField: "symbol",
-      granularity: "hours",
-    },
-  });
+const checkNotInDB = async (ticker: string) => {
+  const db = await connect();
+  let collection = db.collection("S&P500TickerData");
+  const list = await collection.find({ symbol: ticker }).toArray();
+  return list.length === 0;
 };
+
+// const initialize = async () => {
+//   let db = await connect();
+//   db.createCollection("S&P500TickerData", {
+//     timeseries: {
+//       timeField: "date",
+//       metaField: "symbol",
+//       granularity: "hours",
+//     },
+//   });
+// };
 
 const insertData = async (data: IEODPoint[]) => {
   let db = await connect();
@@ -31,17 +42,47 @@ const insertData = async (data: IEODPoint[]) => {
   collection.insertMany(data);
 };
 
+const delayFetch = async (ticker: string, delay: number): Promise<IRawFormat> =>
+  new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(getTicker(ticker));
+    }, delay);
+  });
+
 const processRequest = async (ticker: string) => {
   try {
-    const raw_data = await getTicker(ticker);
+    const raw_data = await delayFetch(ticker, 30000);
     const processed_data = processStream(raw_data);
     insertData(processed_data);
     return "success";
   } catch (e) {
-    console.log(e)
+    console.log(e);
     return "failure";
   }
 };
+
+const doTask = async () => {
+  for (let ticker of tickers) {
+    const response = await checkNotInDB(ticker).then(async (response) => {
+      if (response) {
+        console.log("not in db, getting data now for " + ticker);
+        const done = await processRequest(ticker);
+        console.log(done);
+        if (done === "success") return true;
+        return false;
+      } else {
+        console.log("already exists, no need to refetch");
+        return true;
+      }
+    });
+    if (!response) {
+      console.log("error occurred");
+      break;
+    }
+  }
+};
+
+doTask().then(() => exit());
 
 // initialize(); // DONE
 // const cleanedData = processStream(raw_data); // DONE
@@ -53,3 +94,5 @@ const processRequest = async (ticker: string) => {
 // result.then((res) => {
 //   console.log(res);
 // });
+
+// "Invalid API call. Please retry or visit the documentation (https://www.alphavantage.co/documentation/) for TIME_SERIES_DAILY_ADJUSTED."
